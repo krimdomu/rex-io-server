9acdeeae3196a774875d1b3df9506d58aa6df065
Jan <krimdomu@googlemail.com>
2012-06-03 13:02:24 +0200

    added some docs

--- lib/Rex/IO/Server.pm
+++ lib/Rex/IO/Server.pm
@@ -1,3 +1,56 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+   
+=head1 NAME
+
+Rex::IO::Server - The webservice component of Rex.IO
+
+This is the webservice component of Rex.IO tieing all supported services together under one API.
+
+=head1 GETTING HELP
+
+=over 4
+
+=item * IRC: irc.freenode.net #rex
+
+=item * Bug Tracker: L<https://github.com/krimdomu/rex-io-server/issues>
+
+=back
+
+=head1 INSTALLATION
+
+With cpanminus:
+
+ cpanm Rex::IO::Server
+
+After installing create the file I</etc/rex/io/server.conf>. And set the url to Rex::IO::CMDB.
+
+ {
+    cmdb => "http://rex-cmdb:3000",
+ }
+
+And start the server:
+
+ rex_ioserver daemon
+
+You can also define an other Listen Port (default is 3000)
+
+ rex_ioserver daemon -l 'http://:4000'
+
+Right now there is no Webinterface. You can test if everything is correct with the following command:
+
+ curl -X LIST http://localhost:3000/server
+
+If you get an answer like this it works:
+
+ {"ok":true,"data":{}}
+
+
+=cut
+
 package Rex::IO::Server;
 use Mojo::Base 'Mojolicious';
 
