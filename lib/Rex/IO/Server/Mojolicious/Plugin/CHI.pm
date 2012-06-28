219b35767511327bc2db693f44abc6b3b130f4fb
Jan <krimdomu@googlemail.com>
2012-06-28 23:20:06 +0200

    added chi plugin

---
+++ lib/Rex/IO/Server/Mojolicious/Plugin/CHI.pm
@@ -0,0 +1,32 @@
+#
+# (c) Jan Gehring <jan.gehring@gmail.com>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+
+package Rex::IO::Server::Mojolicious::Plugin::CHI;
+   
+use strict;
+use warnings;
+
+use Mojolicious::Plugin;
+use CHI;
+use base 'Mojolicious::Plugin';
+
+my $cache;
+
+sub register {
+   my ( $plugin, $app ) = @_;
+
+   $app->helper(
+      chi => sub {
+         my $self = shift;
+         if(!$cache) {
+            $cache = CHI->new(driver => "Memory", global => 1);
+         }
+         return $cache;
+      }
+   );
+}
+
+1;
