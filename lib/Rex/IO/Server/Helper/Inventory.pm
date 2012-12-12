1878c57ffd6742f743ed74b2699d1a34af53cfd7
Jan <krimdomu@googlemail.com>
2012-12-12 20:59:40 +0100

    fixed typo

--- lib/Rex/IO/Server/Helper/Inventory.pm
+++ lib/Rex/IO/Server/Helper/Inventory.pm
@@ -133,7 +133,7 @@ sub inventor {
       INVHDDS: for my $hdd ( @{ $ref->{CONTENT}->{STORAGES} } ) {
 
          next INVHDDS unless $hdd;
-         next INVHDDS unless $hdd->{SERIALNUMER};
+         next INVHDDS unless $hdd->{SERIALNUMBER};
 
          if($hdd_dev->serial eq $hdd->{SERIALNUMBER}) {
 
