a976ba26c7b76d06d9619c74d0aae0b36e15fda8
Jan <krimdomu@googlemail.com>
2013-03-19 14:01:18 +0100

    generate passwords

--- bin/generate_user.pl
+++ bin/generate_user.pl
@@ -41,5 +41,5 @@ $bcrypt->add($pass);
 
 my $pw = $bcrypt->hexdigest;
 
-print "INSERT INTO (name, password) VALUES('$user', '$pw')\n";
+print "INSERT INTO user (name, password) VALUES('$user', '$pw')\n";
 
