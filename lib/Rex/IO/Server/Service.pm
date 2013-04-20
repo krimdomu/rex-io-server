3447b73f106d495f40dc961be65313effde8ea7b
Jan <krimdomu@googlemail.com>
2013-04-20 22:37:30 +0200

    run bulk commands

--- lib/Rex/IO/Server/Service.pm
+++ lib/Rex/IO/Server/Service.pm
@@ -59,7 +59,7 @@ sub add_task_to_host {
 
 sub run_task_on_host {
    my $self = shift->render_later;
-   my $redis = Mojo::Redis->new(server => $self->config->{redis}->{server} . ":" . $self->config->{redis}->{port});
+   my $redis = $self->redis;
 
    my $host_id = $self->param("hostid");
    my $task_id = $self->param("taskid");
@@ -154,6 +154,51 @@ sub remove_all_tasks_from_host {
    $self->render_json({ok => Mojo::JSON->true});
 }
 
+sub run_tasks {
+   my ($self) = @_;
+
+   my @tasks = @{ $self->req->json };
+
+   my @ref;
+
+   for my $task (@tasks) {
+      my $task_o = $self->db->resultset("ServiceTask")->find($task->{task_id});
+      my $service_o = $task_o->service;
+      my $host_o = $self->db->resultset("Hardware")->find($task->{server_id});
+
+      if(! $task_o) {
+         return $self->render_json({ok => Mojo::JSON->false}, status => 404, error => "Task not found");
+      }
+
+      if(! $host_o) {
+         return $self->render_json({ok => Mojo::JSON->false}, status => 404, error => "Host not found");
+      }
+
+      push(@ref, {
+         host   => $host_o->name,
+         cmd    => "Execute",
+         script => $service_o->service_name,
+         task   => $task_o->task_name,
+      });
+   }
+
+   my $redis = $self->redis;
+   Mojo::IOLoop->delay(
+      sub {
+         my ($delay) = @_;
+         my $json = Mojo::JSON->new;
+         $redis->publish($self->config->{redis}->{queue} => $json->encode(\@ref), $delay->begin);
+      },
+      sub {
+         $self->render_json({ok => Mojo::JSON->true});
+      }
+   );
+
+   print STDERR Dumper(\@tasks);
+
+   $self->render_json({ok => Mojo::JSON->true});
+}
+
 sub __register__ {
    my ($self, $app) = @_;
    my $r = $app->routes;
@@ -161,10 +206,16 @@ sub __register__ {
    $r->post("/service/:name")->to("service#register");
    $r->post("/service/host/:hostid/task/:taskid")->to("service#add_task_to_host");
    $r->route("/service/host/:hostid/task/:taskid")->via("RUN")->to("service#run_task_on_host");
+   $r->route("/service")->via("RUN")->to("service#run_tasks");
    $r->route("/service")->via("LIST")->to("service#get_all");
    $r->route("/service/:id")->via("LIST")->to("service#get_service");
    $r->route("/service/host/:hostid")->via("LIST")->to("service#get_service_for_host");
    $r->delete("/service/host/:hostid")->to("service#remove_all_tasks_from_host");
+}
+
+sub redis {
+   my ($self) = @_;
+   return Mojo::Redis->new(server => $self->config->{redis}->{server} . ":" . $self->config->{redis}->{port});
 }
 
 1;
