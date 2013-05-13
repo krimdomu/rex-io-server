095dfef1ed8345c55a04e711080e4b030a46f01c
Jan <krimdomu@googlemail.com>
2013-05-13 17:49:16 +0200

    updated db scripts

--- db/0010_add_group_id_to_user.sql
+++ db/0010_add_group_id_to_user.sql
@@ -1,5 +1,5 @@
-INSERT INTO `group` (`id`, `name`) VALUES(1, 'root');
-INSERT INTO `group` (`id`, `name`) VALUES(2, 'nobody');
+INSERT INTO `groups` (`id`, `name`) VALUES(1, 'root');
+INSERT INTO `groups` (`id`, `name`) VALUES(2, 'nobody');
 
-ALTER TABLE `user` ADD COLUMN (`group_id` INT NOT NULL DEFAULT 2);
+ALTER TABLE `users` ADD COLUMN (`group_id` INT NOT NULL DEFAULT 2);
 
