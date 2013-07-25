92e93dc254452804cd01078cdcdcbeb3c246fffc
Jan <krimdomu@googlemail.com>
2013-07-25 10:01:15 +0200

    added local path

--- doc/build.sh
+++ doc/build.sh
@@ -6,7 +6,7 @@ carton install
 
 mkdir rex-io-server-$VER
 mkdir rex-io-server-$VER/doc
-cp -R {bin,db,lib,t} rex-io-server-$VER
+cp -R {bin,db,lib,t,local} rex-io-server-$VER
 cp doc/rex-io-server.init rex-io-server-$VER/doc
 
 tar czf rex-io-server-$VER.tar.gz rex-io-server-$VER
