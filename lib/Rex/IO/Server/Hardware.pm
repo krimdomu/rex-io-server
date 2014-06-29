812be2f9de77dde55af54e96840cf2226c150d2b
Jan <krimdomu@googlemail.com>
2014-06-29 15:54:16 +0200

    hardware updates

--- lib/Rex/IO/Server/Hardware.pm
+++ lib/Rex/IO/Server/Hardware.pm
@@ -54,12 +54,73 @@ sub add {
     );
   }
 
+  # check if hardware already exists
+  my @macs;
+  map { push @macs, $_->{mac} if $_->{mac} } @nw_adapter;
+  $self->app->log->debug( "Searching for macs: " . Dumper( \@macs ) );
+  my @hw = $self->db->resultset("Hardware")->search(
+    {
+      "network_adapters.mac" => { "-in" => \@macs }
+    },
+    {
+      join => "network_adapters",
+    }
+  );
+
+  if (@hw) {
+    $self->app->log->error("Can't add hardware. Hardware already exists.");
+    return $self->render(
+      json => {
+        ok       => Mojo::JSON->false,
+        error    => "Hardware already exists.",
+        redirect => "/1.0/hardware/hardware/" . $hw[0]->id
+      },
+      status => 409
+    );
+  }
+
   try {
     $self->app->log->debug("Creating new hardware...");
+
+    my $os_id = $json->{os_id};
+
+    if ( !$os_id ) {
+
+      # no os exists, create or find one
+      my @kernels = $self->db->resultset("Os")->search(
+        {
+          version => $json->{kernelrelease},
+          kernel  => $json->{kernel},
+        }
+      );
+
+      if ( scalar @kernels == 0 ) {
+        $self->app->log->debug("Need to create new OS.");
+        $self->app->log->debug(
+              "version: $json->{kernelrelease} / kernel: $json->{kernel} "
+            . "/ name: $json->{operatingsystem}" );
+
+        # no kernel exists, create a new one
+        my $new_os = $self->db->resultset("Os")->create(
+          {
+            version => $json->{kernelrelease},
+            kernel  => $json->{kernel},
+            name    => $json->{operatingsystem},
+          }
+        );
+
+        $os_id = $new_os->id;
+      }
+      else {
+        $self->app->log->debug( "Found OS: " . $kernels[0]->id );
+        $os_id = $kernels[0]->id;
+      }
+    }
+
     my $hw = $self->db->resultset("Hardware")->create(
       {
         name              => $json->{name},
-        os_id             => $json->{os_id},
+        os_id             => $os_id,
         uuid              => $json->{uuid} || '',
         server_group_id   => $json->{server_group_id} || 1,
         permission_set_id => $json->{permission_set_id} || 1,
@@ -82,6 +143,7 @@ sub add {
     } @nw_adapter;
 
     for my $nw (@nw_adapter) {
+      next if !$nw->{mac};    # skip nics with no mac
       my $nw_a = $self->db->resultset("NetworkAdapter")->create($nw);
       $self->app->log->debug( "NetworkAdapter created. " . $nw_a->id );
     }
@@ -295,19 +357,188 @@ sub update {
   }
 
   if ($hw) {
+
+################################################################
+
+    my $json = $self->req->json;
+
+    $self->app->log->debug("Updating hardware. ");
+    $self->app->log->debug( Dumper($json) );
+
+    my $mac = $json->{mac};
+    delete $json->{mac};
+
+    my @nw_adapter;
+    if ( exists $json->{network_adapters}
+      && ref $json->{network_adapters} eq "ARRAY" )
+    {
+      @nw_adapter = @{ $json->{network_adapters} };
+      delete $json->{network_adapters};
+    }
+    else {
+      @nw_adapter = (
+        {
+          proto => "dhcp",
+          mac   => $mac,
+          boot  => 1,
+          dev   => "eth0",
+        }
+      );
+    }
+
     try {
-      $self->app->log->debug("Hardware found. going to update.");
-      $hw->update($json);
-      $self->render( json => { ok => Mojo::JSON->true } );
+      $self->app->log->debug("Updating hardware...");
+
+      my $os_id = $json->{os_id};
+
+      if ( !$os_id ) {
+
+        # no os exists, create or find one
+        my @kernels = $self->db->resultset("Os")->search(
+          {
+            version => $json->{kernelrelease},
+            kernel  => $json->{kernel},
+          }
+        );
+
+        if ( scalar @kernels == 0 ) {
+          $self->app->log->debug("Need to create new OS.");
+          $self->app->log->debug(
+                "version: $json->{kernelrelease} / kernel: $json->{kernel} "
+              . "/ name: $json->{operatingsystem}" );
+
+          # no kernel exists, create a new one
+          my $new_os = $self->db->resultset("Os")->create(
+            {
+              version => $json->{kernelrelease},
+              kernel  => $json->{kernel},
+              name    => $json->{operatingsystem},
+            }
+          );
+
+          $os_id = $new_os->id;
+        }
+        else {
+          $self->app->log->debug( "Found OS: " . $kernels[0]->id );
+          $os_id = $kernels[0]->id;
+        }
+      }
+
+      $hw->update(
+        {
+          name              => $json->{name},
+          os_id             => $os_id,
+          uuid              => $json->{uuid} || $hw->uuid,
+          server_group_id   => $json->{server_group_id} || $hw->server_group_id,
+          permission_set_id => $json->{permission_set_id}
+            || $hw->permission_set_id,
+          kernelrelease => $json->{kernelrelease} || $hw->kernelrelease,
+          kernelversion => $json->{kernelversion} || $hw->kernelversion,
+        }
+      );
+
+      $self->app->log->debug( "Hardware updated: " . $hw->id );
+      $self->app->log->debug("Creating hardware adapter for hardware.");
+
+      map {
+        $_->{hardware_id} = $hw->id;
+        $_->{ip}          = ip_to_int( $_->{ip} );
+        $_->{netmask}     = ip_to_int( $_->{netmask} );
+        $_->{network}     = ip_to_int( $_->{network} );
+        $_->{broadcast}   = ip_to_int( $_->{broadcast} );
+        $_->{gateway}     = ip_to_int( $_->{gateway} );
+      } @nw_adapter;
+
+      for my $nw (@nw_adapter) {
+        next if !$nw->{mac};    # skip nics with no mac
+                                # check if adapter exists
+        my @nw_s = $self->db->resultset("NetworkAdapter")->search(
+          {
+            mac => $nw->{mac},
+          }
+        );
+
+        if ( scalar @nw_s >= 1 && $nw_s[0]->hardware_id == $hw->id ) {
+          $self->app->log->debug( "Updating network adapter: " . $nw_s[0]->id );
+          $nw_s[0]->update($nw);
+        }
+        elsif ( scalar @nw_s >= 1 ) {
+          $self->app->log->error(
+            "Found network adapter, but this doesn't belong to hardware.");
+          return $self->render(
+            json => {
+              ok => Mojo::JSON->false,
+              error =>
+                "Found network adapter, but this doesn't belong to hardware."
+            },
+            status => 409
+          );
+        }
+        else {
+          # net network adapter
+          my $nw_a = $self->db->resultset("NetworkAdapter")->create($nw);
+          $self->app->log->debug( "NetworkAdapter created. " . $nw_a->id );
+        }
+      }
+
+      # check for cpus
+      if ( exists $json->{cpus} ) {
+
+        # first delete old cpus
+        $hw->processors->delete;
+
+        for my $cpu ( @{ $json->{cpus} } ) {
+          $cpu->{hardware_id} = $hw->id;
+          my $cpu_a = $self->db->resultset("Processor")->create($cpu);
+          $self->app->log->debug( "Processor created. " . $cpu_a->id );
+        }
+      }
+
+      # check for harddrives
+      if ( exists $json->{harddrives} ) {
+        $hw->harddrives->delete;
+
+        for my $hd ( @{ $json->{harddrives} } ) {
+          $hd->{hardware_id} = $hw->id;
+          my $hd_a = $self->db->resultset("Harddrive")->create($hd);
+          $self->app->log->debug( "Harddrive created. " . $hd_a->id );
+        }
+      }
+
+      # check for memories
+      if ( exists $json->{memories} ) {
+        $hw->memories->delete;
+
+        for my $mem ( @{ $json->{memories} } ) {
+          $mem->{hardware_id} = $hw->id;
+          my $mem_a = $self->db->resultset("Memory")->create($mem);
+          $self->app->log->debug( "Memory created. " . $mem_a->id );
+        }
+      }
+
+      # check for bios information
+      if ( exists $json->{bios} ) {
+        my $bios_s = $hw->bios;
+        $hw->bios->delete if $bios_s;
+
+        my $bios_a = $self->db->resultset("Bios")->create( $json->{bios} );
+        $self->app->log->debug( "Bios created. " . $bios_a->id );
+      }
+
+      $self->render(
+        json => { ok => Mojo::JSON->true, data => $hw->to_hashRef } );
       1;
     }
     catch {
-      $self->app->log->error("Error updating hardware.\n\nERROR: $_\n\n");
+      $self->app->log->error("Error creating new hardware:\n\nERROR: $_\n\n");
       $self->render(
         json   => { ok => Mojo::JSON->false, error => $_ },
         status => 500
       );
     };
+
+################################################################
+
   }
   else {
     $self->app->log->debug("Can't find hardware with id: $hw_id!");
