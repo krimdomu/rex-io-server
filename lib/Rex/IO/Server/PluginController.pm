e7e3ec94c48c4747c30031857c1d38c7742d7e4b
krimdomu <krimdomu@googlemail.com>
2016-02-16 19:33:41 +0100

    some cleanup

--- lib/Rex/IO/Server/PluginController.pm
+++ lib/Rex/IO/Server/PluginController.pm
@@ -10,100 +10,95 @@ use Mojo::Base 'Mojolicious::Controller';
 
 use Data::Dumper;
 
+# this is for hooks
 sub render {
-    my ( $self, @rest ) = @_;
-    $self->app->log->debug("Rendering:");
-    $self->app->log->debug( Dumper( \@rest ) );
+  my ( $self, @rest ) = @_;
+  $self->app->log->debug("Rendering:");
+  $self->app->log->debug( Dumper( \@rest ) );
 
-    my $config = $self->param("config");
-    $self->app->log->debug( "config from BaseController:\n" . Dumper($config) );
+  my $config = $self->param("config");
+  $self->app->log->debug( "config from BaseController:\n" . Dumper($config) );
 
-    my $current_hooks = $self->shared_data("plugin_hooks");
+  my $current_hooks = $self->shared_data("plugin_hooks");
+  $self->app->log->debug( "shared data plugin_hooks from BaseController:\n"
+      . Dumper($current_hooks) );
+
+  my @permissions;
+
+  if ( $self->authenticated ) {
     $self->app->log->debug(
-        "shared data plugin_hooks from BaseController:\n"
-          . Dumper($current_hooks) );
+      "User permissions from BaseController: (" . $self->current_user() . ")" );
+    @permissions =
+      map { $_->name } $self->current_user()->get_permissions();
+    $self->app->log->debug( Dumper( \@permissions ) );
+  }
 
-    my @permissions;
+  if ( $config
+    && exists $current_hooks->{ $config->{plugin} }
+    && exists $current_hooks->{ $config->{plugin} }->{ $config->{url} } )
+  {
+    # we need to call a hook
+    my $plugins =
+      $current_hooks->{ $config->{plugin} }->{ $config->{url} };
 
-    if ( $self->authenticated ) {
-        $self->app->log->debug( "User permissions from BaseController: ("
-              . $self->current_user()
-              . ")" );
-        @permissions =
-          map { $_->name } $self->current_user()->get_permissions();
-        $self->app->log->debug( Dumper( \@permissions ) );
-    }
+    for my $plugin ( @{$plugins} ) {
 
-    if (   $config
-        && exists $current_hooks->{ $config->{plugin} }
-        && exists $current_hooks->{ $config->{plugin} }->{ $config->{url} } )
-    {
-        # we need to call a hook
-        my $plugins =
-          $current_hooks->{ $config->{plugin} }->{ $config->{url} };
+      $self->app->log->debug(
+        "calling plugin from BaseController:\n" . Dumper($plugin) );
 
-        for my $plugin ( @{$plugins} ) {
+      if ( exists $plugin->{location} ) {
+        my $ua          = Mojo::UserAgent->new;
+        my $backend_url = $plugin->{location};
+        $self->app->log->debug("Backend-URL: $backend_url");
 
-            $self->app->log->debug(
-                "calling plugin from BaseController:\n" . Dumper($plugin) );
+        my @params = ( $config->{url} =~ m/:(\w+)/g );
+        $self->app->log->debug(
+          "Found Backend-Params: " . join( ", ", @params ) );
 
-            if ( exists $plugin->{location} ) {
-                my $ua          = Mojo::UserAgent->new;
-                my $backend_url = $plugin->{location};
-                $self->app->log->debug("Backend-URL: $backend_url");
-
-                my @params = ( $config->{url} =~ m/:(\w+)/g );
-                $self->app->log->debug(
-                    "Found Backend-Params: " . join( ", ", @params ) );
-
-                for my $p (@params) {
-                    my $param_data = $self->param($p);
-                    if ( !$param_data ) {
-                        return $self->render(
-                            json => {
-                                ok      => Mojo::JSON->false,
-                                message => "Invalid parameters."
-                            },
-                            status => 500
-                        );
-                    }
-                    $backend_url =~ s/:\Q$p\E/$param_data/;
-                }
-                $backend_url .= "?" . $self->req->url->query;
-                $self->app->log->debug("Parsed-Backend-URL: $backend_url");
-
-                my $meth = $self->req->method;
-                $self->app->log->debug(
-                    "Got Post-Data\n" . Dumper( $self->req->json ) )
-                  if ( $meth eq "POST" );
-
-                my $tx = $ua->build_tx(
-                    $meth => $backend_url => {
-                        "Accept"              => "application/json",
-                        "X-RexIO-Permissions" => join( ",", @permissions ),
-                        "X-RexIO-User"        => $self->current_user()->name,
-                        "X-RexIO-Password" => $self->current_user()->password,
-                        "X-RexIO-Group"    => $self->current_user()->group_id,
-                    } => json => $self->req->json
-                );
-
-                my $resp =
-                  $ua->post(
-                    $backend_url => { "Accept" => "application/json" } =>
-                      json => \@rest );
-                if ( my $res = $resp->success ) {
-                    $self->app->log->debug("Got an answer from $backend_url.");
-                    @rest = @{ $res->json };
-                }
-                else {
-                    $self->app->log->error("Failed calling $backend_url");
-                }
-            }
+        for my $p (@params) {
+          my $param_data = $self->param($p);
+          if ( !$param_data ) {
+            return $self->render(
+              json => {
+                ok      => Mojo::JSON->false,
+                message => "Invalid parameters."
+              },
+              status => 500
+            );
+          }
+          $backend_url =~ s/:\Q$p\E/$param_data/;
         }
+        $backend_url .= "?" . $self->req->url->query;
+        $self->app->log->debug("Parsed-Backend-URL: $backend_url");
 
+        my $req_headers = {
+          "X-RexIO-Permissions" => join( ",", @permissions ),
+          "X-RexIO-User" =>
+            ( $self->current_user() ? $self->current_user()->name : "" ),
+          "X-RexIO-Password" =>
+            ( $self->current_user() ? $self->current_user()->password : "" ),
+          "X-RexIO-Group" =>
+            ( $self->current_user() ? $self->current_user()->group_id : "" )
+          ,
+        };
+
+        my $resp =
+          $ua->post( $backend_url =>
+            { "Accept" => "application/json", %{$req_headers} } =>
+            json => \@rest );
+        if ( my $res = $resp->success ) {
+          $self->app->log->debug("Got an answer from $backend_url.");
+          @rest = @{ $res->json };
+        }
+        else {
+          $self->app->log->error("Failed calling $backend_url");
+        }
+      }
     }
 
-    $self->SUPER::render(@rest);
+  }
+
+  $self->SUPER::render(@rest);
 }
 
 1;
